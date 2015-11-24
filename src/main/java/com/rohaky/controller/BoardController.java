2c5e17e76cd4c6d075bf843035c645acd9a5f334
Seo Seunghyun <sshyun33@gmail.com>
2015-11-24 13:46:53 +0900

    + complete 212p

--- src/main/java/com/rohaky/controller/BoardController.java
+++ src/main/java/com/rohaky/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.rohaky.domain.BoardVO;
 import com.rohaky.service.BoardService;
@@ -25,14 +26,19 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String registPOST(BoardVO board, Model model) throws Exception {
+	public String registPOST(BoardVO board, RedirectAttributes rttr) throws Exception {
 		logger.info("regist post ....");
 		logger.info(board.toString());
 		
 		service.regist(board);
 		
-		model.addAttribute("result", "success");
+		rttr.addFlashAttribute("msg", "success");
 		
-		return "/board/success";
+		return "redirect:/board/listAll";
+	}
+	
+	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
+	public void listAll(Model model) throws Exception {
+		logger.info("show all list....");
 	}
 }
