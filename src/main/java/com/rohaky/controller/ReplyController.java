e0a9e81c4df27073fe2de33aa4e2ddfcbb66d841
sshyun33 <sshyun33@gmail.com>
2016-01-21 18:40:55 +0900

    + complete 388p

--- src/main/java/com/rohaky/controller/ReplyController.java
+++ src/main/java/com/rohaky/controller/ReplyController.java
@@ -4,15 +4,12 @@ import com.rohaky.domain.ReplyVO;
 import com.rohaky.service.ReplyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.ServletRequestBindingException;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.tags.form.SelectTag;
+import org.springframework.http.client.InterceptingClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
-import javax.swing.plaf.basic.BasicScrollPaneUI;
+import java.rmi.server.RMIClassLoader;
+import java.util.List;
 
 /**
  * Created by ilanian on 16. 1. 11.
@@ -37,6 +34,59 @@ public class ReplyController {
             e.printStackTrace();
             entity = new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
-        return  entity;
+        return entity;
+    }
+
+    @RequestMapping(value = "/all/{bno}", method = RequestMethod.GET)
+    public ResponseEntity<List<ReplyVO>> list(
+            @PathVariable("bno") Integer bno) {
+
+        ResponseEntity<List<ReplyVO>> entity = null;
+        try {
+            entity = new ResponseEntity<>(
+                    service.listReply(bno), HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return entity;
+    }
+
+    @RequestMapping(value = "/{rno}",
+            method = {RequestMethod.PUT, RequestMethod.PATCH})
+    public ResponseEntity<String> update(
+            @PathVariable("rno") Integer rno,
+            @RequestBody ReplyVO vo) {
+
+        ResponseEntity<String> entity = null;
+
+        try {
+            vo.setRno(rno);
+            service.modifyReply(vo);
+
+            entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<String>(
+                    e.getMessage(), HttpStatus.BAD_REQUEST
+            );
+        }
+        return entity;
+    }
+
+    @RequestMapping(value = "/{rno}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> remove(
+            @PathVariable("rno") Integer rno) {
+
+        ResponseEntity<String> entity = null;
+        try {
+            service.removeReply(rno);
+            entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return entity;
     }
 }
